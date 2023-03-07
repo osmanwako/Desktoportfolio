71572e31c624a6dda7b75e553bbbb6437bcf9d71
Osman Wako Wario <48554620+osmanwako@users.noreply.github.com>
2023-03-07 20:41:12 +0300

    fixed linter error

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const popupcontainer = document.querySelector('#popupmenu-container')
 
 const hamburgerlink = document.querySelector('#hamburger-link')
@@ -22,3 +23,25 @@ closemenulink.addEventListener('click', closeMenu)
 linkall.forEach(respondlink)
 
 hamburgerlink.addEventListener('click', showmenu)
+=======
+const popupcontainer = document.querySelector("#popupmenu-container");
+const hamburgerlink = document.querySelector("#hamburger-link");
+const closemenulink = document.querySelector("#link-closemenu");
+const linkall = document.querySelectorAll(".navigation-links");
+
+linkall.forEach(respondlink);
+
+hamburgerlink.addEventListener("click", showmenu);
+closemenulink.addEventListener("click", closeMenu);
+
+function showmenu() {
+  popupcontainer.classList.toggle("popupmenu-container");
+}
+function respondlink(link) {
+  link.addEventListener("click", closeMenu);
+}
+
+function closeMenu() {
+  popupcontainer.classList.remove("popupmenu-container");
+}
+>>>>>>> 1eb8a3a (Add Javascript functionality to popup menu)
