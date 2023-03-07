774cd37bea92448b71311e4920135974a5ee3c59
Osman Wako Wario <48554620+osmanwako@users.noreply.github.com>
2023-03-07 20:41:12 +0300

    fixed linter error

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,20 +1,25 @@
-const popupcontainer = document.querySelector("#popupmenu-container");
-const hamburgerlink = document.querySelector("#hamburger-link");
-const closemenulink = document.querySelector("#link-closemenu");
-const linkall = document.querySelectorAll(".navigation-links");
+const popupcontainer = document.querySelector('#popupmenu-container')
 
-linkall.forEach(respondlink);
+const hamburgerlink = document.querySelector('#hamburger-link')
 
-hamburgerlink.addEventListener("click", showmenu);
-closemenulink.addEventListener("click", closeMenu);
+const closemenulink = document.querySelector('#link-closemenu')
 
-function showmenu() {
-  popupcontainer.classList.toggle("popupmenu-container");
-}
-function respondlink(link) {
-  link.addEventListener("click", closeMenu);
+const linkall = document.querySelectorAll('.navigation-links')
+
+function showmenu () {
+  popupcontainer.classList.toggle('popupmenu-container')
 }
 
-function closeMenu() {
-  popupcontainer.classList.remove("popupmenu-container");
+function closeMenu () {
+  popupcontainer.classList.remove('popupmenu-container')
 }
+function respondlink (link) {
+  link.addEventListener('click', closeMenu)
+}
+
+closemenulink.addEventListener('click', closeMenu)
+
+linkall.forEach(respondlink)
+
+hamburgerlink.addEventListener('click', showmenu)
+
