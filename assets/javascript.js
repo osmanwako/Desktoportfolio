61d4e10884aeafee4cb0af7f16b093175a9d3cbf
OsmanFP <osmanwako411@gmail.com>
2023-03-07 21:54:09 +0300

    fix error

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 const popupcontainer = document.querySelector('#popupmenu-container')
 
 const hamburgerlink = document.querySelector('#hamburger-link');
@@ -24,39 +23,3 @@ closemenulink.addEventListener('click', closeMenu);
 linkall.forEach(respondlink)
 
 hamburgerlink.addEventListener('click', showmenu)
-=======
-const popupcontainer = document.querySelector("#popupmenu-container");
-const hamburgerlink = document.querySelector("#hamburger-link");
-const closemenulink = document.querySelector("#link-closemenu");
-const linkall = document.querySelectorAll(".navigation-links");
-=======
-const popupcontainer = document.querySelector('#popupmenu-container')
->>>>>>> 774cd37 (fixed linter error)
-
-const hamburgerlink = document.querySelector('#hamburger-link')
-
-const closemenulink = document.querySelector('#link-closemenu')
-
-const linkall = document.querySelectorAll('.navigation-links')
-
-function showmenu () {
-  popupcontainer.classList.toggle('popupmenu-container')
-}
-
-function closeMenu () {
-  popupcontainer.classList.remove('popupmenu-container')
-}
-<<<<<<< HEAD
->>>>>>> 1eb8a3a (Add Javascript functionality to popup menu)
-=======
-function respondlink (link) {
-  link.addEventListener('click', closeMenu)
-}
-
-closemenulink.addEventListener('click', closeMenu)
-
-linkall.forEach(respondlink)
-
-hamburgerlink.addEventListener('click', showmenu)
-
->>>>>>> 774cd37 (fixed linter error)
