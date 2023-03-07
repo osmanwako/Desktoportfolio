1eb8a3aa548ea04b948e523cad06cc7eb621acbb
BENSON NJUGUNA <108814163+Bennyjoez@users.noreply.github.com>
2023-03-07 18:18:50 +0300

    Add Javascript functionality to popup menu

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,1 +1,20 @@
+const popupcontainer = document.querySelector("#popupmenu-container");
+const hamburgerlink = document.querySelector("#hamburger-link");
+const closemenulink = document.querySelector("#link-closemenu");
+const linkall = document.querySelectorAll(".navigation-links");
 
+linkall.forEach(respondlink);
+
+hamburgerlink.addEventListener("click", showmenu);
+closemenulink.addEventListener("click", closeMenu);
+
+function showmenu() {
+  popupcontainer.classList.toggle("popupmenu-container");
+}
+function respondlink(link) {
+  link.addEventListener("click", closeMenu);
+}
+
+function closeMenu() {
+  popupcontainer.classList.remove("popupmenu-container");
+}
